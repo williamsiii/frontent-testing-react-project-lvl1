a2b7c5de466a94f249e87000e69c28f9206bfdef
ArtemN <a.nosov@hh.ru>
2021-05-05 12:14:43 +0300

    initial stuff

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const fs = require("fs");
+
+const params = {
+    output: process.cwd()
+}
+
+
+const processArgs = () => {
+    const args = process.argv
+    for (let i = 0; i < args.length; i++) {
+        if (args[i] === "--output") {
+            if (args[i + 1] && fs.existsSync(args[i + 1])) {
+                params.output = args[i + 1]
+            } else {
+                Error("Destination directory doesn't exist. Downloading page to current directory.")
+            }
+            break;
+        }
+    }
+
+
+    console.log(`File will be downloaded to ${params.output}`);
+}
+
+
+function main() {
+    processArgs();
+
+}
+
+
+module.exports = main
