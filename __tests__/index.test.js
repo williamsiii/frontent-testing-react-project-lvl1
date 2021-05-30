ba529e8b4ba0c15a03e4a239e4b7a601b8985488
ArtemN <a.nosov@hh.ru>
2021-05-30 14:11:48 +0300

    less tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,117 +1,47 @@
 import nock from 'nock';
-import os from 'os';
-import { readFile, rm } from 'fs/promises';
+import { readFile, readdir } from 'fs/promises';
 import 'axios-debug-log';
-import { PageLoader } from '../src/index';
-
-
-describe('page-loader, setup', () => {
-  test('download folder, default', async () => {
-    await PageLoader.main();
-    expect(PageLoader.params.output).toEqual(PageLoader.INIT_STATE.output);
-  });
-
-  test('download folder, home directory', async () => {
-    await PageLoader.main(os.homedir());
-    expect(PageLoader.params.output).toEqual(`${os.homedir()}/`);
-  });
-
-  test('download folder, wrong folder', async () => {
-    await PageLoader.main('/var/spoon/bar/tron/mud/jizz');
-    expect(PageLoader.params.output).toEqual(PageLoader.INIT_STATE.output);
-  });
-
-  test('url, default', async () => {
-    await PageLoader.main();
-    expect(PageLoader.params.url).toEqual(PageLoader.defaultUrl);
-  });
-
-  test('url, argument', async () => {
-    await PageLoader.main(undefined, 'https://yandex.ru');
-    expect(PageLoader.params.url).toEqual('https://yandex.ru');
-  });
-});
-
-describe('page-loader, fetch', () => {
-  let fixture = null;
-  beforeAll(async () => {
-    process.argv = process.argv.slice(0, 2);
-    fixture = await readFile('./__fixtures__/index.html', 'utf8');
-  });
-
-  test('fetch page', async () => {
-    const scope = nock('https://example.com')
-      .get('/')
-      .reply(200, fixture);
-    await PageLoader.main(undefined, 'https://example.com/');
-    expect(PageLoader.params.response).toEqual(fixture);
-    scope.done();
-  });
-});
+import main from '../src/index';
 
 describe('page-loader, parse response', () => {
   let scope;
-  let scope1;
-  let scope2;
-  let scope3;
   let fixture = null;
+  const testDir = `${process.cwd()}/hexlet-io-_files`;
   beforeAll(async () => {
     fixture = await readFile('./__fixtures__/index.html', 'utf8');
   });
 
   beforeEach(() => {
-    PageLoader.params.response = null;
-    PageLoader.params.url = 'https://hexlet.io';
-    PageLoader.params.fileName = 'test_folder';
-    PageLoader.params.resourcesDir = null;
-    PageLoader.params.resources = [];
-    PageLoader.params.resourcesFileNames = [];
-    PageLoader.params.originalResources = [];
     process.argv = process.argv.slice(0, 2);
-    scope = nock('https://hexlet.io')
-      .get('/')
+    scope = nock(/hexlet\.io/)
+      .persist()
+      .get(/\/.{0,}/)
       .reply(200, fixture);
-    scope1 = nock('https://hexlet.io')
-      .get(/\/.{1,}/)
-      .reply(200, 'some bytes');
-    scope2 = nock('https://ru.hexlet.io')
-      .get(/\/.{1,}/)
-      .reply(200, 'some bytes');
-    scope3 = nock('https://cdn2.hexlet.io')
-      .get(/\/.{1,}/)
-      .reply(200, 'some bytes');
   });
 
   test('parse resources, default', async () => {
-    await PageLoader.main(undefined, 'https://hexlet.io/');
-    expect(PageLoader.params.resources).toEqual(
-      [
-        '/assets/professions/nodejs.png',
-        '/assets/menu.css',
-        '/assets/application.css',
-        '/packs/js/runtime.js',
-      ],
-    );
+    await main(process.cwd(), 'https://hexlet.io/');
+    const res = await readdir(testDir);
+    expect(res).toMatchObject(expect.arrayContaining([
+      '-assets-professions-nodejs.png',
+      '-assets-menu.css',
+      '-assets-application.css',
+      '-packs-js-runtime.js',
+    ]));
   });
 
-  test('saved files not empty', async () => {
-    await PageLoader.main();
-    PageLoader.params.resourcesFileNames.forEach(async (file) => {
-      const ff = await readFile(file, 'utf8');
+  test('file contents', async () => {
+    await main(process.cwd(), 'https://hexlet.io/');
+    const res = await readdir(testDir);
+    res.forEach(async (file) => {
+      const path = `${testDir}/${file}`;
+      const ff = await readFile(path, 'utf8');
       expect(ff && ff.length).toBeTruthy();
     });
   });
 
   afterAll(async () => {
-    PageLoader.params.response = null;
-    PageLoader.params.resources = [];
-    PageLoader.params.resourcesFileNames = [];
-    PageLoader.params.originalResources = [];
-    await rm(PageLoader.params.resourcesDir, { recursive: true, force: true }, () => { });
     scope.done();
-    scope1.done();
-    scope2.done();
-    scope3.done();
   });
 });
 
@@ -121,7 +51,7 @@ describe('page-loader, failures', () => {
       .get('/')
       .reply(400);
     const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { });
-    await PageLoader.main(undefined, 'https://example.com');
+    await main(undefined, 'https://example.com');
     expect(mockExit).toHaveBeenCalled();
     scope.done();
   });
