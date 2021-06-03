2c09fb6f229adb738eba22a2d2d35080e02dae7b
ArtemN <a.nosov@hh.ru>
2021-06-03 22:14:33 +0300

    everything to promises

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,7 @@
 import nock from 'nock';
+import path from 'path';
+import os from 'os';
+import { promises as fs } from 'fs';
 import { readFile, readdir } from 'fs/promises';
 import 'axios-debug-log';
 import main from '../src/index';
@@ -6,53 +9,59 @@ import main from '../src/index';
 describe('page-loader, parse response', () => {
   let scope;
   let fixture = null;
-  const testDir = `./downloads/hexlet-io-_files`;
-  beforeAll(async () => {
-    fixture = await readFile('./__fixtures__/index.html', 'utf8');
-  });
+  let pathToTempDir;
 
-  beforeEach(() => {
-    process.argv = process.argv.slice(0, 2);
-    scope = nock(/hexlet\.io/)
+  beforeAll(async () => {
+    nock.disableNetConnect();
+    fixture = await readFile('./__fixtures__/index.html', 'utf8');
+    pathToTempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'test-'));
+    scope = nock(/hexlet/)
       .persist()
       .get(/\/.{0,}/)
       .reply(200, fixture);
   });
 
-  test('parse resources, default', async () => {
-    await main('./downloads/', 'https://hexlet.io/');
-    const res = await readdir(testDir);
-    expect(res).toMatchObject(expect.arrayContaining([
-      '-assets-professions-nodejs.png',
-      '-assets-menu.css',
-      '-assets-application.css',
-      '-packs-js-runtime.js',
-    ]));
+  test('fetch resources', async () => {
+    await main('https://hexlet.io/', pathToTempDir);
+
+    const res = await readFile(`${pathToTempDir}/hexlet-io-.html`);
+    expect(res && res.length).toBeTruthy();
   });
 
   test('file contents', async () => {
-    await main('./downloads/', 'https://hexlet.io/');
-    const res = await readdir(testDir);
-    res.forEach(async (file) => {
-      const path = `${testDir}/${file}`;
+    const page = await readFile(`${pathToTempDir}/hexlet-io-.html`, 'utf8');
+    expect(page && page.length).toBeTruthy();
+    const resResources = await readdir(`${pathToTempDir}/hexlet-io-_files`);
+    expect(resResources).toMatchObject(expect.arrayContaining([
+      'hexlet-io-assets-professions-nodejs.png',
+      'hexlet-io-assets-application.css',
+    ]));
+    resResources.forEach(async (file) => {
+      const path = `${pathToTempDir}/hexlet-io-_files/${file}`;
       const ff = await readFile(path, 'utf8');
       expect(ff && ff.length).toBeTruthy();
     });
   });
 
   afterAll(async () => {
+    nock.cleanAll();
     scope.done();
   });
 });
 
-// describe('page-loader, failures', () => {
-//   test('fetch page failed', async () => {
-//     const scope = nock('https://example.com')
-//       .get('/')
-//       .reply(400);
-//     const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { });
-//     await main(undefined, 'https://example.com');
-//     expect(mockExit).toHaveBeenCalled();
-//     scope.done();
-//   });
-// });
+describe('page-loader, failures', () => {
+  let pathToTempDir;
+  beforeAll(async () => {
+    nock.disableNetConnect();
+    pathToTempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'test-fail-'));
+  })
+  test('fetch page failed', async () => {
+    const scope = nock('https://example.com')
+      .get('/')
+      .reply(400);
+    await expect(
+      main('https://example.com', pathToTempDir),
+    ).rejects.toThrow('Request failed with status code 400');
+    scope.done();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,225 +1,110 @@
-import * as stream from 'stream';
-import { promisify } from 'util';
 import axios from 'axios';
-import fs, { constants } from 'fs';
-import { writeFile, access, mkdir } from 'fs/promises';
+import path from 'path';
+import { createWriteStream, promises as fs } from 'fs';
 import cheerio from 'cheerio';
-import 'axios-debug-log';
+import { keys } from 'lodash';
 
-const defaultUrl = 'http://www.tsjdom18.ru';
-const INIT_STATE = {
-  output: './downloads/',
-  url: defaultUrl,
-  fileName: 'default.html',
-  response: null,
-  resources: [],
-  resourcesFileNames: [],
-  originalResources: [],
-  resourcesDir: null,
+
+const RESOURCES = {
+  link: 'href',
+  img: 'src',
+  script: 'src',
 };
 
-const params = { ...INIT_STATE };
-
-const linkCondition = ($, el) => $(el).attr('rel') !== 'stylesheet';
-
-const RESOURCES = [
-  { tag: 'img', attr: 'src' },
-  { tag: 'link', attr: 'href', condition: linkCondition },
-  { tag: 'script', attr: 'src' },
-];
-
-const accessPath = async (path) => {
-  let res = false;
+const composeName = (link) => {
+  const rename = (q) => q.replace(/^\//, '').replace(/[^a-z1-9]/g, '-');
   try {
-    await access(path, constants.W_OK);
-    res = true;
-  } catch (err) {
-    console.error(err);
+    const { host = '', pathname } = new URL(link);
+    return rename(host + pathname);
+  } catch (e) {
+    return rename(link);
   }
-  return res;
 };
 
-const composeName = (name, withExtension = false) => {
-  let extension = '';
-  let res;
-  if (withExtension) {
-    const queryParts = name.split('?');
-    const parts = queryParts[0].split('.');
-    [res] = [queryParts];
-    if (parts.length > 1) {
-      extension = `.${parts.pop()}`;
-      res = parts.join('.');
+const composeLink = (link, type = 'file') => {
+  switch (type) {
+    case 'file': {
+      const ext = path.extname(link) || '.html';
+      const withoutExt = link.replace(ext, '');
+      return composeName(withoutExt) + ext;
     }
-  } else {
-    res = name;
-  }
-  if (Array.isArray(res)) {
-    [res] = res;
-  }
-  res = res
-    .replace(/(http|https):\/\/(www\.)?/g, '')
-    .replace(/[\W_]/g, '-');
-  return res + extension;
-};
-
-const checkOptions = async () => {
-  if (params.output[params.output.length - 1] !== '/') {
-    params.output += '/';
-  }
-  const isExist = await accessPath(params.output);
-  if (!isExist) {
-    params.output = INIT_STATE.output;
-  }
-  params.fileName = composeName(params.url);
-};
-
-const fetchPage = async () => {
-  let resp;
-  try {
-    resp = await axios.get(params.url);
-  } catch (err) {
-    console.error('Не получилось скачать страницу:', err.code);
-  }
-
-  if (resp && resp.status === 200) {
-    params.response = resp.data;
-  } else {
-    params.response = null;
-    console.error('Не получилось скачать страницу');
+    case 'directory':
+      return `${composeName(link)}_files`;
+    default:
+      throw new Error(`Unknown type: ${type}`);
   }
 };
 
-const changeSource = () => {
-  const $ = cheerio.load(params.response);
-  RESOURCES.forEach((resource) => {
-    $(resource.tag).each((index, res) => {
-      const src = $(res).attr(resource.attr);
-      const q = params.originalResources.indexOf(src);
-      if (q !== -1) {
-        $(res).attr(resource.attr, params.resourcesFileNames[q]);
-      }
+const parseResourceLinks = (page, dir, baseUrl) => {
+  const { host, origin } = new URL(baseUrl);
+  const linksArr = [];
+  const $ = cheerio.load(page);
+  keys(RESOURCES).forEach((tag) => {
+    $(tag).each((index, element) => {
+      const uri = $(element).attr(RESOURCES[tag]);
+
+      const link = new URL(uri, origin);
+      if (link.host !== host) return;
+
+      linksArr.push(link);
+      $(element).attr(
+        RESOURCES[tag],
+        path.join(dir, composeLink(link.toString())),
+      );
     });
   });
-  params.response = $.html();
+  return { result: $.html(), linksArr };
 };
 
-const checkSaveDirectory = async () => {
-  // create dir if not exists
-  try {
-    params.resourcesDir = `${params.output}${params.fileName}_files`;
-    const exists = await accessPath(params.resourcesDir);
-    if (!exists) {
-      await mkdir(params.resourcesDir, (err) => {
-        if (err) {
-          console.error('Не получилось создать директорию!', err);
-        }
-      });
-    }
-  } catch (err) {
-    console.error('Не получилось создать директорию', err);
-  }
-};
-
-const parseForElement = async (element, attr, condition) => {
-  checkSaveDirectory();
-  const $ = cheerio.load(params.response);
-  const coreDomain = params.url.replace(/^(http|https):\/\//, '');
-  const arr = [];
-  const origArr = [];
-  const filesArr = [];
-  $(element).each((index, el) => {
-    const originalAttr = $(el).attr(attr);
-
-    // проверка на доп условия
-    if (!originalAttr || (condition && condition($, el))) {
-      return;
-    }
-    const attrib = originalAttr.split('?')[0];
-    if (attrib) {
-      const res = attrib
-        .replace(/^(http|https):\/\//, '') // сначала убираю протокол
-        .replace(coreDomain, '///') // теперь убираю основной домен, заменяя его спецпоследовательностью
-        .replace(/^.*\/\/\//, '/') // теперь убираю все верхние домены, если есть спецпоследовательность
-        .replace(/\/{2,}/g, '/'); // заменяю все последовательности '/.../' на одинарный '/'
-
-      if (res.match(/^\//) && res.length < 240) {
-        const targetFileName = composeName(res, true);
-        filesArr.push(`${params.resourcesDir}/${targetFileName}`);
-        arr.push(res);
-        if (originalAttr[0] === '/') {
-          origArr.push(`${params.url}${originalAttr}`);
-        } else {
-          origArr.push(originalAttr);
-        }
-      }
-    }
-  });
-  params.resources = params.resources.concat(arr);
-  params.originalResources = params.originalResources.concat(origArr);
-  params.resourcesFileNames = params.resourcesFileNames.concat(filesArr);
-  // save resources
-  arr.forEach(async (item) => {
-    const finished = promisify(stream.finished);
-    const index = arr.indexOf(item);
-    const writer = fs.createWriteStream(filesArr[index]);
-    return axios({
-      method: 'get',
-      url: origArr[index],
-      responseType: 'stream',
+const loadResource = (url, resourceOutputPath) => {
+  const resultFilePath = path.join(resourceOutputPath, composeLink(url));
+  return axios({
+    method: 'get',
+    url,
+    responseType: 'stream',
+  })
+    .then(({ data }) => {
+      data.pipe(createWriteStream(resultFilePath));
     })
-      .then(async (response) => {
-        response.data.pipe(writer);
-        return finished(writer);
+    .catch((error) => {
+      throw error;
+    });
+};
+
+const saveResources = (url, resourceOutputPath, linksArr) => {
+  const resultDirName = composeLink(url, 'directory');
+  const resultOutput = path.join(resourceOutputPath, resultDirName);
+  return fs
+    .mkdir(resultOutput)
+    .then(() => {
+      return linksArr.map((link) => {
+        const resourceUrl = new URL(link, url);
+        return loadResource(resourceUrl.toString(), resultOutput);
+      });
+    })
+    .then((tasks) => Promise.all(tasks))
+    .catch((error) => {
+      throw error;
+    });
+};
+
+const main = (baseUrl, outputPath) => {
+  return axios.get(baseUrl).then((res) => {
+    const htmlFileName = `${composeName(baseUrl)}.html`
+    const resultFilePath = path.join(outputPath, htmlFileName);
+    const page = res.data;
+    const sourceDir = composeLink(baseUrl, 'directory');
+    const { result, linksArr } = parseResourceLinks(page, sourceDir, baseUrl);
+
+    return fs
+      .writeFile(resultFilePath, result, { encoding: 'utf8', flag: "w" })
+      .then(() => {
+        saveResources(baseUrl, outputPath, linksArr)
       })
-      .catch((err) => {
-        console.error(origArr[index], err.code);
+      .catch((error) => {
+        throw error;
       });
   });
-};
-
-const parsePage = async () => {
-  try {
-    await RESOURCES.map(async (res) => {
-      await parseForElement(res.tag, res.attr, res.condition);
-    });
-    changeSource();
-  } catch (err) {
-    console.error('Не получилось распарсить документ', err);
-  }
-};
-
-const savePage = async () => {
-  if (params.output[params.output.length - 1] !== '/') {
-    params.output += '/';
-  }
-  params.finalPath = `${params.output}${params.fileName}.html`;
-  try {
-    await writeFile(params.finalPath, params.response);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const main = async (output, url) => {
-  params.output = output || INIT_STATE.output;
-  params.url = url || INIT_STATE.url;
-  checkOptions();
-  await fetchPage();
-  //
-  if (!params.response) {
-    console.error('Нет данных для обработки');
-    return;
-  }
-  //
-  await parsePage();
-  savePage();
-};
-
-export const PageLoader = {
-  main,
-  defaultUrl,
-  INIT_STATE,
-  params,
 };
 
 export default main;
